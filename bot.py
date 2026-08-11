110f6f688242581166d24ee698249fec0e7348e3
siliconeanubhav-debug <siliconeanubhav@gmail.com>
2026-08-11 12:55:28 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from config import Config
+
+app = Client(
+    "ACSearchBot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
